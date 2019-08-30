31d44e2d3993f6af463ae538c81000c1097d5d02
AaronMillOro <aaron.ibt@gmail.com>
2019-08-30 20:05:41 +0200

    Models changes, addition of user authentication

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, g, jsonify, render_template
 
 import config
+import models
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/api/v1/todos/', methods=['GET'])
 def my_todos():
-    return render_template('index.html')
+    return jsonify({'todos': todos})
 
 if __name__ == '__main__':
+    models.initialize()
     app.run(debug=config.DEBUG, host=config.HOST, port=config.PORT)
--- models.py
+++ models.py
@@ -1,13 +1,70 @@
 import datetime
 
+from argon2 import PasswordHasher
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
                           BadSignature, SignatureExpired)
 from peewee import *
 
 import config
 
+DATABASE = SqliteDatabase('todos.sqlite')
+HASHER = PasswordHasher()
+
+
+class User(Model):
+    username= CharField(unique=True)
+    email= CharField(unique=True)
+    password= CharField()
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_user(cls, username, email, password, **kwargs):
+        email = email.lower()
+        try:
+            cls.select().where(
+                (cls.email==email)|(clc.username**username)
+            ).get()
+        except cls.DoesNotExist:
+            user= cls(username=username, email=email)
+            user.password = user.set_password(password)
+            user.save()
+            return user
+        else:
+            raise Exception('Username or email already registered')
+
+    @staticmethod
+    def verif_auth_token(token):
+        serializer = Serializer(config.SECRET_KEY)
+        try:
+            data = serializer.loads(token)
+        except (SignatureExpired, BadSignature):
+            return None
+        else:
+            user = User.get(User.id==data['id'])
+            return user
+
+    @staticmethod
+    def set_password(password):
+        return HASHER.hash(password)
+
+    def verif_password(self, password):
+        return HASHER.verify(self.password, password)
+
+    def generate_auth_token(self, expires=2000):
+        serializer = Serializer(config.SECRET_KEY, expires_in=expires)
+        return serializer.dumps({'id': self.id})
+
+
+class Todo(Model):
+    name= CharField()
+    done= BooleanField(default=False)
+
+    class Meta:
+        database = DATABASE
 
 def initialize():
-    DATABASE.connect(reuse_if_open=True)
+    DATABASE.connect()
     DATABASE.create_tables([Todo], safe=True)
     DATABASE.close()
